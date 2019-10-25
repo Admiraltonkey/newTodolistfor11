575c45a0e6f9d5f4b43ff4053bc1e7ad91c6bbaf
Admiraltonkey <AdmiralTonkey@gmail.com>
2019-10-25 16:28:37 +0300

    Updates

---
+++ precache-manifest.69e8cbeb28d8429cb4edeb5f315c9031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbfda6d7ad3e31ad1915fefd4c0e5e86",
+    "url": "/newTodolistfor11/index.html"
+  },
+  {
+    "revision": "da2629db915a4dac38a6",
+    "url": "/newTodolistfor11/static/css/main.a1da7050.chunk.css"
+  },
+  {
+    "revision": "29a61891538a08392ee6",
+    "url": "/newTodolistfor11/static/js/2.c76ed9f1.chunk.js"
+  },
+  {
+    "revision": "da2629db915a4dac38a6",
+    "url": "/newTodolistfor11/static/js/main.b8a32a50.chunk.js"
+  },
+  {
+    "revision": "108af1420569d83d1b9f",
+    "url": "/newTodolistfor11/static/js/runtime~main.06b21249.js"
+  }
+]);
